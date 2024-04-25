1b6c9d7bc7b423ad53b0cd737568fee7a3823494
Shraman Das <shraman99@gmail.com>
2024-04-25 09:48:12 +0530

    Adding file exists check

--- src/main/java/com/craft/demo/utils/fileUtils/FileUtilities.java
+++ src/main/java/com/craft/demo/utils/fileUtils/FileUtilities.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.FileNotFoundException;
+
 
 @Component
 public class FileUtilities {
@@ -22,6 +24,15 @@ public class FileUtilities {
     }
     public long getLastAlteredTimestamp()
     {
-        return fileGetter.getFile(fileLocation).lastModified();
+        try {
+            System.out.println("Getting file at " + fileLocation);
+            if(fileGetter.getFile(fileLocation).exists())
+            return fileGetter.getFile(fileLocation).lastModified();
+            else
+                throw new FileNotFoundException("File not found!");
+        } catch (Exception e) {
+            System.err.println("File not found!");
+            throw new RuntimeException(e);
+        }
     }
 }
